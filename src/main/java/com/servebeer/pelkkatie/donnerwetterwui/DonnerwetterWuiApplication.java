0ec053733f7d14200031656aa55c468146016f32
unknown <tommi.aihkisalo@gmail.com>
2017-09-25 19:33:24 +0300

    Initial commit

---
+++ src/main/java/com/servebeer/pelkkatie/donnerwetterwui/DonnerwetterWuiApplication.java
@@ -0,0 +1,12 @@
+package com.servebeer.pelkkatie.donnerwetterwui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonnerwetterWuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonnerwetterWuiApplication.class, args);
+	}
+}
